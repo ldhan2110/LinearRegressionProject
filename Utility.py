c69f92464ae2c1f6f0d7ca88e678265c6ac8fd78
Le_Dang_Hoang_An <49611930+ldhan2110@users.noreply.github.com>
2020-06-06 22:47:55 +0700

    Update Utility.py

--- Utility.py
+++ Utility.py
@@ -4,6 +4,7 @@ from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, LabelBinarizer
 
+clf = linear_model.LinearRegression()
 
 # Lấy các cột có kiểu dữ liệu "object" đưa vào 1 Data Frame mới
 def get_obj_types(df):
@@ -21,6 +22,8 @@ def transform_manufacture_transmisson(obj_df):
     obj_df["engineType"] = lb_make.fit_transform(obj_df["engineType"])
     obj_df["color"] = lb_make.fit_transform(obj_df["color"])
     obj_df["model"] = lb_make.fit_transform(obj_df["model"])
+    obj_df["drivetrain"] = lb_make.fit_transform(obj_df["drivetrain"])
+    obj_df["bodyType"] = lb_make.fit_transform(obj_df["bodyType"])
     obj_df["feature_0"] = lb_make.fit_transform(obj_df["feature_0"])
     obj_df["feature_1"] = lb_make.fit_transform(obj_df["feature_1"])
     obj_df["feature_2"] = lb_make.fit_transform(obj_df["feature_2"])
@@ -37,7 +40,7 @@ def transform_manufacture_transmisson(obj_df):
 
 # Hàm chuyển cột "body types"
 def transform_bodyType_drivetrain(obj_df):
-    obj_df = pd.get_dummies(obj_df, columns=["bodyType", "drivetrain"], prefix=["body", "drive"])
+    obj_df = pd.get_dummies(obj_df, columns=["bodyType","drivetrain","manufacturer","model"], prefix=["body","drive","manu","model"])
     return obj_df
 
 
@@ -47,7 +50,17 @@ def read_file_csv(path):
     return raw_data
 
 
-clf = linear_model.LinearRegression()
+
+def RMSE(x_test,y_test):
+    sum = 0
+    result = clf.predict(x_test)
+    y_test = y_test.to_numpy()
+    for i in range(0,len(x_test)):
+        sum+=(result[i]-y_test[i])**2
+    return np.math.sqrt(sum/len(x_test))
+
+
+
 
 X_train = read_file_csv("res\\X_train.csv")
 Y_train = read_file_csv("res\\Y_train.csv")
@@ -55,18 +68,17 @@ Y_train = read_file_csv("res\\Y_train.csv")
 X_transform = transform_manufacture_transmisson(X_train)
 X_transform = transform_bodyType_drivetrain(X_transform)
 
-#X_train, X_test, y_train, y_test = train_test_split(X_transform, Y_train, test_size=0.2, random_state=0)
-#X = X_transform["engineCapacity"].values.reshape(-1,1)
-y = Y_train['price'].values.reshape(-1,1)
-#clf.fit(X, y)
-
-
-print(np.any(np.isnan(X_transform["engineCapacity"])))
-print(np.all(np.isfinite(X_transform["engineCapacity"])))
-
 X_transform["engineCapacity"] = X_transform['engineCapacity'].fillna(0)
+X_transform = X_transform.drop(columns = ["id","photos"])
+Y_train = Y_train.drop(columns = "id")
 
-clf.fit(X_transform, Y_train)
+#Chia tập dữ liệu thành 2 phần
+X_train, X_test, y_train, y_test = train_test_split(X_transform, Y_train, test_size=0.2, random_state=0)
 
-print(clf.intercept_)
+
+clf.fit(X_train, y_train)
+
 print(clf.coef_)
+print(clf.predict(X_test))
+print(RMSE(X_test,y_test))
+
